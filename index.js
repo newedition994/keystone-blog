7efdd8c4dc989193aafca0cc073b86acce16a0b1
Breyanna Vass <breyannavass@gmail.com>
2021-02-22 17:01:24 -0500

    adding process env and gitignore

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
+const dotenv = require('dotenv').config()
 const { Keystone } = require('@keystonejs/keystone');
 const { GraphQLApp } = require('@keystonejs/app-graphql');
 const { AdminUIApp } = require('@keystonejs/app-admin-ui');
 const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
 const PROJECT_NAME = 'Keystone Blog';
-const adapterConfig = { mongoUri: 'mongodb+srv://Vass:leekota79@cluster0.stgcr.mongodb.net/keystoneblog?retryWrites=true&w=majority' };
+const adapterConfig = { mongoUri: process.env.MONGO_URI, };
 
 
-/**
- * You've got a new KeystoneJS Project! Things you might want to do next:
- * - Add adapter config options (See: https://keystonejs.com/keystonejs/adapter-mongoose/)
- * - Select configure access control and authentication (See: https://keystonejs.com/api/access-control)
- */
 
 const keystone = new Keystone({
   adapter: new Adapter(adapterConfig),
+  cookieSecret: process.env.COOKIE_SECRET,
 });
 
 module.exports = {
